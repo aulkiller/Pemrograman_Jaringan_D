a609c4f5f37c583820106015f151218795606bde
aulkiller <58225261+aulkiller@users.noreply.github.com>
2021-04-07 03:34:45 +0700

    test pertama,local sukses

---
+++ progjar1/tugas 1/client2mb.py
@@ -0,0 +1,37 @@
+import sys
+import socket
+
+# Listening Port
+portz = ['5000', '5002']
+
+for i in portz:
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('192.168.161.128', i)
+    print(f"connecting to {server_address}")
+    sock.connect(server_address)
+
+    try:
+        # Send data
+        messagesize = 2097152
+        message = ''
+        for x in range(messagesize // 16):
+            message += 'INI ADALAH DATA!'
+        print(f"sending INI ADALAH DATA! sebanyak ", messagesize // 16, "kali")
+        sock.sendall(message.encode())
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+        print(len(message))
+        msgcount = 0;
+        while amount_received < (amount_expected + 12):
+            data = sock.recv(16)
+            amount_received += len(data)
+            msgcount += 1
+            print(f"{data}")
+    finally:
+        print("closing")
+        print("Jumlah", {data}, "yang diterima ada", msgcount, "kali")
+        sock.close()
---
+++ progjar1/tugas 1/clientphoto.py
@@ -0,0 +1,36 @@
+import sys
+import socket
+import os
+
+# Listening Port
+portz = ['5000', '5002']
+
+for i in portz:
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('192.168.161.128', i)
+    print(f"connecting to {server_address}")
+    sock.connect(server_address)
+
+    try:
+        # Send data
+        img = "ups.png"
+        imgfile = open(img, 'rb')
+        imgbase = imgfile.read()
+        print(f"sending {img} with size", len(imgbase) // 1024, "kb")
+        sock.sendall(imgbase)
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(imgbase)
+        img_response = "image_response_port" + i + ".png"
+        with open(img_response, 'wb') as response:
+            while amount_received < (amount_expected + 12):
+                data = sock.recv(16)
+                amount_received += len(data)
+                response.write(data)
+    finally:
+        print("closing")
+        print("respons balik berupa foto", img_response, "dapat dibuka")
+        sock.close()
